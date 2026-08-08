9643e9ef8d21344fc7a41dffa55a242e588cb99b
Will Byrne <will.alex.byrne@gmail.com>
2026-08-08 21:59:29 +0100

    [elysia-server] add happy path test for pokemon endpoint

--- elysia-server/src/server.test.ts
+++ elysia-server/src/server.test.ts
@@ -1,16 +1,23 @@
 import { describe, expect, it, mock } from 'bun:test';
+import { PokeboxEntry } from './model';
 
 const mockPokedex = [
   { id: 1, name: 'bulbasaur' },
   { id: 25, name: 'pikachu' }
 ];
 
+const mockPokemon: PokeboxEntry = {
+    id: 1,
+    name: 'Bulbasaur',
+    species_description: 'Its a bulbasaur',
+    types: ['grass'],
+    sprites: undefined,
+    abilities: [{ name: 'overgrow' , flavour_text: 'Ups GRASS moves in a pinch.', effect: 'When this Pokémon has 1/3 or less of its HP remaining, its Grass-type moves inflict 1.5× as much regular damage.'}]
+}
+
 mock.module('./pokebox', () => ({
   getPokedex: mock(async () => mockPokedex),
-  getPokemon: mock(async (idOrName: string) => ({
-    id: Number(idOrName),
-    name: 'pikachu',
-  })),
+  getPokemon: mock(async (idOrName: string) => mockPokemon),
 }));
 
 const { app } = await import('./server');
@@ -30,5 +37,12 @@ describe('Server', () => {
 
     expect(response.status).toBe(200);
     expect(await response.json()).toEqual(mockPokedex);
+  });
+
+  it('returns a pokemon when presented a valid id', async () => {
+    const response = await app.handle(new Request('http://localhost/pokemon/1'));
+
+    expect(response.status).toBe(200);
+    expect(await response.json()).toEqual(mockPokemon);
   })
 })
